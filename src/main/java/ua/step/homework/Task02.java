1522871a5043bfdb9dff48b2a48d9a788844a0e3
Michael <michaelllebowski@gmail.com>
2020-09-29 00:35:55 +0300

    Все задания в Task02 выполнены

--- src/main/java/ua/step/homework/Task02.java
+++ src/main/java/ua/step/homework/Task02.java
@@ -1,5 +1,7 @@
 package ua.step.homework;
 
+import javax.crypto.spec.PSource;
+
 /**
  * В массиве хранится n явно заданных текстовых строк.
  * <p>
@@ -13,7 +15,22 @@ package ua.step.homework;
  */
 public class Task02 {
     public static void main(String[] args) {
-        // TODO: Проверяйте методы здесь
+        /*String[] string = new String[] {"Hello", "Privet", "Good"};
+        String str = join(string);
+        System.out.println(str);*/
+
+        /*String arr = "z";
+        String str = "a";
+
+        if(arr.charAt(0)>str.charAt(0))
+        {
+            System.out.println(arr);
+        }
+        else
+        {
+            System.out.println(str);
+        }*/
+
     }
 
     /**
@@ -23,8 +40,16 @@ public class Task02 {
      * @return строка состоящая из элементов массив
      */
     public static String join(String[] strings) {
-        // TODO: удалите исключение и пишите здесь код
-        throw new RuntimeException("Not implemented yet");
+        String new_str = "";
+        for(int i = 0; i<strings.length; i++)
+        {
+            new_str = new_str + strings[i];
+            if(i!=strings.length-1)
+            {
+                new_str = new_str + " ";
+            }
+        }
+        return new_str;
     }
 
     /**
@@ -35,8 +60,16 @@ public class Task02 {
      * @return строка состоящая из элементов массива
      */
     public static String join(String[] strings, String glue) {
-        // TODO: удалите исключение и пишите здесь код
-        throw new RuntimeException("Not implemented yet");
+        String new_str = "";
+        for(int i = 0; i<strings.length; i++)
+        {
+            new_str = new_str + strings[i];
+            if(i!=strings.length-1)
+            {
+                new_str = new_str + glue;
+            }
+        }
+        return new_str;
     }
 
     /**
@@ -45,8 +78,20 @@ public class Task02 {
      * @param strings - массив строк для сортировки
      */
     public static void sortDesc(String[] strings) {
-        // TODO: удалите исключение и пишите здесь код
-        throw new RuntimeException("Not implemented yet");
+
+        for(int k =0; k< strings.length; k++)
+        {
+            for(int i = 0; i<strings.length-1; i++)
+            {
+                if(strings[i].charAt(0)<strings[i+1].charAt(0))
+                {
+                    String tmp = strings[i+1];
+                    strings[i+1] = strings[i];
+                    strings[i] = tmp;
+                }
+            }
+        }
+
     }
 
     /**
@@ -55,7 +100,52 @@ public class Task02 {
      * @param strings - массив строк для сортировки
      */
     public static void sortByWordCount(String[] strings) {
-        // TODO: удалите исключение и пишите здесь код
-        throw new RuntimeException("Not implemented yet");
+        for(int k =0; k< strings.length; k++)
+        {
+            for (int i = 0; i < strings.length - 1; i++)
+            {
+                int counter = 0;
+                for (int j = 0; j < strings[i].length(); j++)
+                {
+                    if (strings[i].charAt(j) == ' ')
+                    {
+                        counter++;
+                    }
+                }
+
+                counter = num(strings, counter, i);
+
+                if (counter == 1)
+                {
+                    String tmp = strings[i+1];
+                    strings[i+1] = strings[i];
+                    strings[i] = tmp;
+                }
+            }
+        }
     }
+
+    public static int num(String[] strings, int counter_1, int i)
+    {
+        int counter_2 = 0;
+        for(int j = 0; j<strings[i+1].length(); j++)
+        {
+            if(strings[i+1].charAt(j) == ' ')
+            {
+                counter_2++;
+            }
+        }
+
+        if(counter_1 > counter_2)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
 }
+
+
